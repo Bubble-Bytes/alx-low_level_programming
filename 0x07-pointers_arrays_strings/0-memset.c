f68606434902e561b5681849c9b082377cf5bf5a
Bubble-Bytes <evelynboora@gmail.com>
2023-10-23 10:37:13 -0700

    0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * _memset - fills memory with a constant byte
+ *
+ * @s: pointer to string
+ * @b: constant byte
+ * @n: first bytes to change
+ * Return: pointer to modified string
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
