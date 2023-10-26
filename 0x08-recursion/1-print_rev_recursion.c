f26d8bf723e94a7ce309d11ae0918f71a5e00380
Bubble-Bytes <evelynboora@gmail.com>
2023-10-26 11:07:51 -0700

    Missing comments

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 
 /**
- * printing a string in reverse
- * string to print
+ *_print_rev_recursion -  prints a string in reverse
+ * @s: the string to be printed
  */
 void _print_rev_recursion(char *s)
 {
