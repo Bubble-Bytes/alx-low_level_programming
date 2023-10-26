b40e6f878e78b9f00efa6f4ac20f8355708ddbea
Bubble-Bytes <evelynboora@gmail.com>
2023-10-26 12:39:26 -0700

    5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,24 +1,33 @@
 #include "main.h"
-#include <stdio.h>
 
+int actual_sqrt_recursion(int n, int i);
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to calculate the square root of
+ *
+ * Return: the resulting square root
+ */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
-	{
-		return -1;
-	}
-
-	int result = 0;
-	while (result * result <= n)
-	{
-		result++;
-	}
-
-	return result -1;
+		return (-1);
+	return (actual_sqrt_recursion(n, 0));
 }
 
-int main()
+/**
+ * actual_sqrt_recursion - recurses to find the natural
+ * square root of a number
+ * @n: number to calculate the sqaure root of
+ * @i: iterator
+ *
+ * Return: the resulting square root
+ */
+int actual_sqrt_recursion(int n, int i)
 {
-	printf("The natural square root of 16 is %i\n", _sqrt_recursion(n));
-	return 0;
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (actual_sqrt_recursion(n, i + 1));
 }
